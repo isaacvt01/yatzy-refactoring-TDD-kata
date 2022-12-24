ebc7311839e2eea9c434b412ffa1575a29aac218
isaacvt01 <_vasqueztucent@cifpfbmoll.eu>
2022-12-24 03:25:15 +0100

    (rafactor) Refactorizar el método Three of a kind

--- yatzy.py
+++ yatzy.py
@@ -105,7 +105,7 @@ class Yatzy:
         return 0
 
     @staticmethod
-    def two_pair(*dice):
+    def two_of_a_kind(*dice):
         dice = list(dice)
         dice = sorted(dice, reverse=True)
         for die in dice:
@@ -131,16 +131,14 @@ class Yatzy:
         return 0
 
     @staticmethod
-    def three_of_a_kind(d1, d2, d3, d4, d5):
-        t = [0] * 6
-        t[d1 - 1] += 1
-        t[d2 - 1] += 1
-        t[d3 - 1] += 1
-        t[d4 - 1] += 1
-        t[d5 - 1] += 1
-        for i in range(6):
-            if (t[i] >= 3):
-                return (i + 1) * 3
+    def three_of_a_kind(*dice):
+        dice = list(dice)
+        dice = sorted(dice, reverse=True)
+        for die in dice:
+            if dice.count(die) == 3:
+                return die * 3
+            else:
+                continue
         return 0
 
     @staticmethod
